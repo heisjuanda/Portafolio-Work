46a5130c1a971ac934cff4f9b008923c56374024
Juan David Moreno Alfonso <jdma253@gmail.com>
2022-11-02 11:03:50 -0500

    another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import * as Aos from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Work';
+  mostrar: boolean = false;
+  ngOnInit() {
+    setTimeout(() => {
+      this.mostrar = true;
+      Aos.init();
+      Aos.refresh();
+    }, 100);
+  }
 }
